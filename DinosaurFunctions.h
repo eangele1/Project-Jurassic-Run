7b38cf6308f57b11b7b2650122f68f581ed4f7c4
eangele1 <ezerangeles1@gmail.com>
2018-12-04 12:03:19 -0800

    Updated DinosaurFunctions

--- DinosaurFunctions.h
+++ DinosaurFunctions.h
@@ -1,85 +1,35 @@
-#ifndef DinasaurFunctions_h
-#define DinasaurFunctions_h
-#include <iostream>
-#include "DinausarFunctions.cpp"
-#include <vector>
+#ifndef DinosaurFunctions_h
+#define DinosaurFunctions_h
 
-class Moves
-{
+class Moves{
 private:
     float x = 0.0; //x Coordinate
     float y = 0.0; //y Coordinate
     
 public:
-    bool Left;
-    bool Right;
     bool Crouch;
     bool Jump;
+
     //Constructor
-    Moves()
-    {
-        this->x= x;
-        this->y= y;
-    }
-    //Constructor for moving left and right
-    Moves(float X)
-    {
-        this->x= x;
-        Left = false;
-        Crouch = false;
-    }
+    Moves(){}
+
     //Contructor for moving up and down
-    Moves(float x, float y)
-    {
+    Moves(float x, float y){
         this->x = x;
         this->y = y;
-        Right = false;
         Jump = false;
     }
-    float getX()const
-    {
-        return this->x;
-    }
-    float getY()const
-    {
-        return this->y;
-    }
-    void setX(float x)
-    {
-        this->x =x;
-    }
-    void setY(float y)
-    {
-        this->y = y;
-    }
-    
-    void Dinoleft()
-    {
-        float x = Moves->getX();
-        x= x - .05;
-        
-    }
-    void Dinoright()
-    {
-        float x = Moves->getX();
-        x = x + .05;
-    }
-    void crouch()
-    {
-        float y= Moves->getY();
-        y = -.05;
-        
-    }
-    void jump()
-    {
-        float y = Moves->getY();
-        float x = Moves->getX();
-        y = y + 1.0;
-        y = x + .05;
-        
-    }
-};
 
-vector <Moves*> functions;
+    float getX()const;
+    float getY()const;
+    void setX(float x);
+    void setY(float y);
+
+    void crouch();
+    void jump();
+    
+    //Destructor
+    ~Moves(){}
+};
 
 #endif
