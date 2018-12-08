b4401becacdfaf80352dc681fc1269ba22d92b37
eangele1 <ezerangeles1@gmail.com>
2018-12-08 14:40:44 -0800

    Updates to DinosaurFunctions

--- DinosaurFunctions.h
+++ DinosaurFunctions.h
@@ -1,9 +1,5 @@
-#ifndef DinasaurFunctions_h
-#define DinasaurFunctions_h
 #include <iostream>
-//#include "DinausarFunctions.cpp"
 #include <vector>
-//#include "App.cpp"
 #ifndef DinosaurFunctions_h
 #define DinosaurFunctions_h
 
@@ -37,34 +33,5 @@ public:
     //Destructor
     ~Moves(){}
 };
-
-float Moves::getX()const{
-    return this->x;
-}
-
-float Moves::getY()const{
-    return this->y;
-}
-
-void Moves::setX(float x){
-    this->x = x;
-}
-
-void Moves::setY(float y){
-    this->y = y;
-}
-
-void Moves::crouch(){
-    float y = getY();
-    y = -.05;
-    
-}
-void Moves::jump(){
-    float y = getY();
-    float x = getX();
-    y = y + 1.0;
-    y = x + .05;
-}
-
 
 #endif
